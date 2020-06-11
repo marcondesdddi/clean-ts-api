2f4ebc635a2f4a30fdf6be4e443d94ce0553c8e0
Diego Gabriel <marcondesdddi@gmail.com>
2020-06-11 19:17:44 -0300

    chore: add eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
       'standard' // Out of the box StandardJS rules
     ],
     'plugins': [
-      '@typescript-eslint' // Let's us override rules below.
+      '@typescript-eslint', // Let's us override rules below.
+      'jest'
     ],
     'rules': {
       '@typescript-eslint/no-use-before-define': 'off', // Allows us to hoist variables and functions which I am a fan of, functions not variables that is.
@@ -27,6 +28,11 @@ module.exports = {
       '@typescript-eslint/indent': 'off', // This is the job of StandardJS, they are competing rules so we turn off the Typescript one. 
       'no-unused-vars': 'off', // On the fence about using this one, sometimes we import a package that is never used directly. 
       'node/no-unsupported-features/es-syntax': 'off', // Allows us to use Import and Export keywords.
-      'eslint-disable-next-line no-undef': 'off'
+      'no-undef': 'off',
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     }
   }
