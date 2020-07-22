cecb7d399da70af4949ddfbe9217eab519314532
Diego Gabriel <marcondesdddi@gmail.com>
2020-07-22 19:19:16 -0300

    refactor: move password comparation to the ValidationComposite

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -125,20 +125,20 @@ describe('SignUp Controller', () => {
 	// 	expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordConfirmation')))
 	// })
 
-	test('Should return 400 if password confirmation fails', async () => {
-		// sut: system under test
-		const { sut } = makeSut()
-		const httpRequest = {
-				body: {
-						name: 'any_name',
-						email: 'any_email@mail.com',
-						password: 'any_password',
-						passwordConfirmation: 'invalid_password'
-				}
-		}
-		const httpResponse = await sut.handle(httpRequest)
-		expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
-	})
+	// test('Should return 400 if password confirmation fails', async () => {
+	// 	// sut: system under test
+	// 	const { sut } = makeSut()
+	// 	const httpRequest = {
+	// 			body: {
+	// 					name: 'any_name',
+	// 					email: 'any_email@mail.com',
+	// 					password: 'any_password',
+	// 					passwordConfirmation: 'invalid_password'
+	// 			}
+	// 	}
+	// 	const httpResponse = await sut.handle(httpRequest)
+	// 	expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
+	// })
 
 	test('Should return 400 if an invalid email is provided', async () => {
 		// sut: system under test
