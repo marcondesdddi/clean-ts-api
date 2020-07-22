1e03ed22f3eec851d25115d13d484dc08cfdb1de
Diego Gabriel <marcondesdddi@gmail.com>
2020-07-22 20:47:44 -0300

    fix: path module

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -2,7 +2,7 @@ import { ValidationComposite } from '../../presentation/helpers/validators/valid
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
-import { EmailValidatorAdapter } from '@/utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
