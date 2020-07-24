0cadc234994b395c1550e3b2fe9188aa7707d235
Diego Gabriel <marcondesdddi@gmail.com>
2020-07-24 11:21:50 -0300

    test: ensure DbAuthentication throws id HashComparer if throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -80,4 +80,11 @@ describe('DbAuthentication UseCase', () => {
 		await sut.auth(makeFakeAuthentication())
 		expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
 	})
+
+	test('Should throw if HashComparer throws', async () => {
+		const { sut, hashComparerStub } = makeSut()
+		jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+		const promise = sut.auth(makeFakeAccount())
+		await expect(promise).rejects.toThrow()
+	})
 })
