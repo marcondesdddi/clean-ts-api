017ab2967994bc748a5a5414bf3edd6ff581bd9d
Diego Gabriel <marcondesdddi@gmail.com>
2020-07-25 12:48:04 -0300

    refactor: rename Encrypter to Hasher

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -1,5 +1,5 @@
 import { CompareFieldsValidation } from './compare-fields-validation'
-import { InvalidParamError } from '@/presentation/errors'
+import { InvalidParamError } from '../../errors'
 
 const makeSut = (): CompareFieldsValidation => {
 	return new CompareFieldsValidation('field', 'fieldToCompare')
