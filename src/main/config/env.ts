76612165d85081ff45adae65267b0cde6e4ae5c9
Diego Gabriel <marcondesdddi@gmail.com>
2020-07-28 17:02:05 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
 	port: process.env.PORT || 5050,
 	jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
-	mongoUrl: process.env.MONGO_URL || 'mongodb://root:root@127.0.0.1:27017/admin'
+	mongoUrl: process.env.MONGO_URL || 'mongodb://root:root@mongo:27017/admin'
 }
